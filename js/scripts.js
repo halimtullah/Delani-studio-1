da141c3bd90a618260df7a5fc71404637f6b9dfc
khadija <sumeyahassan34@gmail.com>
2020-02-24 09:53:47 +0300

    Add changes index.html

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("form").submit(function(event){
+      var name=$("input#name").val();
+    alert("Hello "+name+" Thank you for contacting us");
+    event.preventDefault();
+    });
+   });
+  
