ee4d0f423ee2506861e6c9589b41927ecb7970df
khadija <sumeyahassan34@gmail.com>
2020-02-25 04:09:38 +0300

    Add changes styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
+    $(".icon1").click(function(){
+      $(".show1").toggle();
+    });
+  });
+  $(document).ready(function(){
+    $(".icon2").click(function(){
+      $(".show2").toggle();
+    });
+  });
+  $(document).ready(function(){
+    $(".icon3").click(function(){
+      $(".show3").toggle();
+    });
+  });
+ $(document).ready(function(){
     $("form").submit(function(event){
       var name=$("input#name").val();
       var email=$("input#email").val();
